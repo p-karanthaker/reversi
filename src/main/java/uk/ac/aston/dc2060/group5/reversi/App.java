e83db080f00bcfa7b434fe589bb11f09eaf7435b
Karan Thaker <karan.thaker@capgemini.com>
2016-10-04 14:42:32 +0100

    Initial Commit
    Creation of the Reversi project with sample dummy class and unit test.

---
+++ src/main/java/uk/ac/aston/dc2060/group5/reversi/App.java
@@ -0,0 +1,13 @@
+package uk.ac.aston.dc2060.group5.reversi;
+
+public class App {
+	
+	private App() {
+		
+	}
+	
+	public static String getGreeting() {
+		return "Hello";
+	}
+
+}
---
+++ src/test/java/uk/ac/aston/dc2060/group5/reversi/AppTest.java
@@ -0,0 +1,13 @@
+package uk.ac.aston.dc2060.group5.reversi;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+	
+	@Test
+	public void testGetGreetingMethod() {
+		Assert.assertEquals("Hello", App.getGreeting());
+	}
+
+}
